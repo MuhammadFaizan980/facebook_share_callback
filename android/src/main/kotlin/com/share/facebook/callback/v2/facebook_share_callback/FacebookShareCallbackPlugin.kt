7503f6188629f949544e5c6eb26d27c6edc50ff8
MuhammadFaizan980 <mohammadfaizan980@gmail.com>
2025-05-10 23:37:50 +0300

    refactor: update dependencies and code structure
    Updates the project dependencies and reorganizes the code in `FacebookShareCallbackPlugin.kt`. This includes adding `ActivityAware` and `BinaryMessenger` imports and adjusting the order of existing imports. The core functionality remains the same.

--- android/src/main/kotlin/com/share/facebook/callback/v2/facebook_share_callback/FacebookShareCallbackPlugin.kt
+++ android/src/main/kotlin/com/share/facebook/callback/v2/facebook_share_callback/FacebookShareCallbackPlugin.kt
@@ -1,45 +1,49 @@
 package com.share.facebook.callback.v2.facebook_share_callback
 
-
 import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.content.pm.ResolveInfo
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
-import android.os.Bundle
-import android.provider.MediaStore
+import android.graphics.Bitmap
 import android.text.TextUtils
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
 
 import androidx.annotation.NonNull
 import androidx.core.content.FileProvider
 
+import com.facebook.share.model.SharePhoto
+import com.facebook.share.model.SharePhotoContent
+import com.facebook.share.widget.ShareDialog
+
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+
+import java.io.File
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
 import com.facebook.CallbackManager
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+
+
+import io.flutter.plugin.common.PluginRegistry.Registrar
+import io.flutter.plugin.common.BinaryMessenger
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.share.Sharer
 import com.facebook.share.model.ShareLinkContent
-import com.facebook.share.model.SharePhoto
-import com.facebook.share.model.SharePhotoContent
 import com.facebook.share.widget.MessageDialog
-import com.facebook.share.widget.ShareDialog
-
-import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.embedding.engine.plugins.activity.ActivityAware
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
-import io.flutter.plugin.common.BinaryMessenger
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
+import android.database.Cursor
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import io.flutter.plugin.common.PluginRegistry
-import io.flutter.plugin.common.PluginRegistry.Registrar
-
 
 
 /** FacebookShareCallbackPlugin */
