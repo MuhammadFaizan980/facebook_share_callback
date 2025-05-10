9a705f4bb60e51b92fc9b101945674292d97f040
MuhammadFaizan980 <mohammadfaizan980@gmail.com>
2025-05-11 00:28:13 +0300

    refactor: update plugin interface implementation
    Updates the `FacebookShareCallbackPlugin` class to directly implement `ActivityPluginBinding.ActivityResultListener` instead of using the fully qualified name `io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding.ActivityResultListener`. This simplifies the code without changing functionality.

--- android/src/main/kotlin/com/share/facebook/callback/v2/facebook_share_callback/FacebookShareCallbackPlugin.kt
+++ android/src/main/kotlin/com/share/facebook/callback/v2/facebook_share_callback/FacebookShareCallbackPlugin.kt
@@ -45,8 +45,7 @@ import android.widget.ImageView
 
 
 /** FacebookShareCallbackPlugin */
-class FacebookShareCallbackPlugin : FlutterPlugin, MethodCallHandler, ActivityAware,
-    io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding.ActivityResultListener {
+class FacebookShareCallbackPlugin : FlutterPlugin, MethodCallHandler, ActivityAware,ActivityPluginBinding.ActivityResultListener {
     /// The MethodChannel that will the communication between Flutter and native Android
     ///
     /// This local reference serves to register the plugin with the Flutter Engine and unregister it
