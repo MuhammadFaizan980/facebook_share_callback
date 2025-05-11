9401706df0914fbc2d15f1fde7dc6e54bd0ce6f0
MuhammadFaizan980 <mohammadfaizan980@gmail.com>
2025-05-11 11:35:46 +0300

    refactor: improve iOS Facebook share dialog handling
    This commit refactors the iOS implementation to improve the handling of the Facebook share dialog.
    
    - Adds conditional checks using `ShareDialog.canShow` before attempting to display the share dialog for both link and photo sharing.
    - Logs an error and notifies the result if the share dialog cannot be shown on the current device (e.g., simulator).
    - Adjusts imports to be in alphabetical order.

--- ios/Classes/FacebookShareCallbackPlugin.swift
+++ ios/Classes/FacebookShareCallbackPlugin.swift
@@ -1,17 +1,18 @@
+import FBSDKShareKit
 import Flutter
 import UIKit
-import FBSDKShareKit
 
 public class FacebookShareCallbackPlugin: NSObject, FlutterPlugin, SharingDelegate {
-    
+
     var result: FlutterResult?
-    
+
     public static func register(with registrar: FlutterPluginRegistrar) {
-        let channel = FlutterMethodChannel(name: "share_facebook_callback", binaryMessenger: registrar.messenger())
+        let channel = FlutterMethodChannel(
+            name: "share_facebook_callback", binaryMessenger: registrar.messenger())
         let instance = FacebookShareCallbackPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
-    
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         self.result = result
         if call.method == "getPlatformVersion" {
@@ -23,7 +24,7 @@ public class FacebookShareCallbackPlugin: NSObject, FlutterPlugin, SharingDelega
                 let shareUrl = arguments["url"] as? String ?? ""
                 let uint8Image = arguments["uint8Image"] as? FlutterStandardTypedData
                 _ = arguments["imageName"] as? String ?? ""
-                
+
                 switch type {
                 case "ShareType.shareLinksFacebook":
                     shareLinksFacebook(withQuote: shareQuote, withUrl: shareUrl)
@@ -35,60 +36,76 @@ public class FacebookShareCallbackPlugin: NSObject, FlutterPlugin, SharingDelega
             }
         }
     }
-    
+
     // MARK: - SharingDelegate Methods
     @objc public func sharer(_ sharer: Sharing, didCompleteWithResults results: [String: Any]) {
         NSLog("--------------------success")
         self.result?("success")
     }
-    
+
     @objc public func sharer(_ sharer: Sharing, didFailWithError error: Error) {
         NSLog("-----------------onError")
         self.result?("error")
     }
-    
+
     @objc public func sharerDidCancel(_ sharer: Sharing) {
         NSLog("-----------------onCancel")
         self.result?("cancel")
     }
-    
+
     private func shareLinksFacebook(withQuote quote: String?, withUrl urlString: String?) {
         DispatchQueue.main.async {
             let viewController = UIApplication.shared.delegate?.window??.rootViewController
             let shareContent = ShareLinkContent()
-            
+
             if let url = urlString, let covertURL = URL(string: url) {
                 shareContent.contentURL = covertURL
             }
-            
+
             if let quoteString = quote {
                 shareContent.quote = quoteString
             }
-            
-            let shareDialog = ShareDialog(viewController: viewController, content: shareContent, delegate: self)
-            shareDialog.show()
+
+            let shareDialog = ShareDialog(
+                viewController: viewController, content: shareContent, delegate: self)
+            if ShareDialog.canShow(ShareLinkContent.self) {
+                shareDialog.show()
+            } else {
+                NSLog("Facebook ShareDialog cannot be shown on this device")
+                self.result?("error")  // Or a better error message like "not supported on simulator"
+            }
+            //            shareDialog.show()
         }
     }
-    
-    private func sharePhotoFacebook(withuint8Image uint8Image: FlutterStandardTypedData?, withQuote quote: String?) {
+
+    private func sharePhotoFacebook(
+        withuint8Image uint8Image: FlutterStandardTypedData?, withQuote quote: String?
+    ) {
         DispatchQueue.main.async {
             guard let data = uint8Image else {
                 self.result?("error: No image data provided")
                 return
             }
-            
+
             let viewController = UIApplication.shared.delegate?.window??.rootViewController
             guard let uiImage = UIImage(data: data.data) else {
                 self.result?("error: Invalid image data")
                 return
             }
-            
+
             let photo = SharePhoto(image: uiImage, isUserGenerated: true)
             let content = SharePhotoContent()
             content.photos = [photo]
-            
-            let shareDialog = ShareDialog(viewController: viewController, content: content, delegate: self)
-            shareDialog.show()
+
+            let shareDialog = ShareDialog(
+                viewController: viewController, content: content, delegate: self)
+            if ShareDialog.canShow(ShareLinkContent.self) {
+                shareDialog.show()
+            } else {
+                NSLog("Facebook ShareDialog cannot be shown on this device")
+                self.result?("error")  // Or a better error message like "not supported on simulator"
+            }
+//            shareDialog.show()
         }
     }
 }
